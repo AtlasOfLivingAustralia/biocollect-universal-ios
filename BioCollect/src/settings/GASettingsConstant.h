8623fee1964b779f78dc3ce6e1253f50939c9c64
sat01a <sathish.sathyamoorthy@gmail.com>
2016-03-02 15:00:45 +1100

    BioCollect iPad version initial checkin

---
+++ BioCollect/src/settings/GASettingsConstant.h
@@ -0,0 +1,20 @@
+//
+//  GASettingsConstant.h
+//  GreenArmy
+//
+//  Created by Sathya Moorthy, Sathish (CSIRO IM&T, Clayton) on 11/04/2014.
+//  Copyright (c) 2014 Sathya Moorthy, Sathish (CSIRO IM&T, Clayton). All rights reserved.
+//
+
+#ifndef GreenArmy_GASettingsConstant_h
+#define GreenArmy_GASettingsConstant_h
+
+/*
+ #define REST_SERVER @"http://fieldcapture-dev.ala.org.au"
+ #define REST_SERVER @"https://fieldcapture-test.ala.org.au"
+*/
+
+#define REST_SERVER @"https://fieldcapture-test.ala.org.au"
+#define ECODATA_SERVER @"https://ecodata-test.ala.org.au"
+
+#endif
