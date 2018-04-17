9391fa1bf1127286d7209663cc1e2c69b70d00bb
temi <temi.varghese@csiro.au>
2018-04-17 17:06:48 +1000

    Larger image preview

---
+++ BioCollect/src/Controller/FXFormLargeImagePickerCell.h
@@ -0,0 +1,13 @@
+//
+//  FXFormLargeImagePickerCell.h
+//  BioCollect
+//
+//  Created by Varghese, Temi (PI, Black Mountain) on 17/4/18.
+//  Copyright © 2018 Sathya Moorthy, Sathish (CSIRO IM&T, Clayton). All rights reserved.
+//
+#import "FXForms.h"
+
+@interface FXFormLargeImagePickerCell : FXFormImagePickerCell {
+    UIImageView* imagePickerView;
+}
+@end
